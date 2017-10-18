0af877e2eaf17566e91cb84d9e5b658c4295b4b8
Connor Kendrick <connorkendrickmusic@gmail.com>
2017-10-18 18:20:40 -0400

    Store username in local Chrome storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,66 @@
-var input = document.getElementById("user-input");
-input.addEventListener("keydown", function(event) {
-    if (event.keyCode === 13 && input.value != '') {
-        var welcomeMessage = document.getElementById('welcome-message');
-        var initialLandingMessage = document.getElementById('initial-landing-message');
-
-        welcomeMessage.innerHTML = "Hello, " + input.value + ".";
-        welcomeMessage.style.display = 'inline';
-
-        hide(initialLandingMessage);
-        show(welcomeMessage);
-    }
-});
-
 function hide(ele) {
     ele.style.display = 'none';
 }
 
+
 function show(ele) {
     ele.style.display = 'block';
 }
+
+
+function displayInitialLanding() {
+    show(document.getElementById('initial-landing'));
+}
+
+
+function displayUserLanding(username) {
+    hide(document.getElementById('initial-landing'));
+    
+    document.getElementById('welcome-message').innerHTML = "Hello, " + username + ".";
+    
+    show(document.getElementById('user-landing'));
+}
+
+
+function load() {
+    chrome.storage.local.get('username', function(result) {
+        show(document.getElementById('cover'));
+        
+        if (typeof result.username === 'undefined') {
+            displayInitialLanding();
+        }
+        else {
+            displayUserLanding(result.username);
+        }
+    });
+}
+
+
+function save(username, callback) {
+    chrome.storage.local.set({'username': username});
+    callback();
+}
+
+
+var input = document.getElementById('user-input');
+input.addEventListener('keydown', function(event) {
+    if (event.keyCode === 13 && input.value != '') {
+        save(input.value, function() {
+            load();
+        });
+    }
+});
+
+
+load();
+//chrome.storage.local.remove('username');
+//chrome.storage.local.set({'username': 'connorkendrick'});
+//var obj = chrome.storage.local.get('username', function(result) {
+//    if (typeof result.username === 'undefined') {
+//        alert("This is undefined");
+//    }
+//    else {
+//        alert("This is defined");
+//        alert(result.username);
+//    }
+//});
